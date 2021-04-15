8afc9dd85a635b6f34892a256f2fca22a29aad13
unknown <rafcasto@gmail.com>
2021-04-15 13:04:46 +1200

    adding report

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -3,7 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features")
+@CucumberOptions(features = "classpath:features",plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"},monochrome = false)
 public class IntegrationTest
 {
 
